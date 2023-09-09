20e23e3f37f536bdaf932ce5138e648b18a89cc4
Dustin McMahon <dpmcmahon85@gmail.com>
2023-09-09 16:58:35 -0400

    Hash Map v1

---
+++ ObjFreqHashMap.java
@@ -0,0 +1,118 @@
+//import java.util.*;
+import java.io.*;
+
+public class ObjFreqHashMap implements java.io.Serializable {
+
+    static final class Node {
+        public Object key;
+        public int freq;
+        public Node next;
+
+        public Node(Object key, Node next){
+            this.next = next;
+            this.key = key;
+            this.freq = 1;
+        }
+        public String toString(){
+            return key.toString() + "appears " + freq + " times.";
+        }
+    }
+
+    Node[] map;
+    int count;
+
+    public ObjFreqHashMap(){
+        this(8);
+    }
+
+    public ObjFreqHashMap(int initialSize){
+        this.map = new Node[initialSize];
+        count = 0;
+    }
+
+    /**
+     * 
+     * @param key
+     * @return either null or the Node containing the key
+     */
+    public Node get(Object key){
+
+        for(Node e = map[getIndex(key)]; e != null; e = e.next){
+            if(e.key.equals(key)) return e;
+        }
+        return null;
+    }
+
+    public boolean contains(Object key){
+
+        for(Node e = map[getIndex(key)]; e != null; e = e.next){
+            if(e.key.equals(key)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param key
+     * 
+     * the way i am using this is to count unique words
+     * if a key exists inclement that keys freq
+     * if it does not exist, add a new one to the front of that index
+     */
+    public void add(Object key){
+        Node tempNode = get(key);
+        if (tempNode != null){
+            tempNode.freq++;
+        } else {
+            int i = getIndex(key);
+            tempNode = new Node(key, map[i]);
+            map[i] = tempNode;
+            count++;
+        }
+    }
+
+    public void remove(Object key){
+        int i = getIndex(key);
+        Node current = map[i], prior = null;
+        while(current != null){
+            if(key.equals(current.key)){
+                if(prior == null) map[i] = map[i].next;
+            } else {
+                prior.next = current.next;
+                break;
+            }
+            prior = current;
+            current = current.next;
+        }
+    }
+
+    public void printAll(){
+        for(int i = 0; i < map.length; i++ ){
+            for(Node e = map[i]; e != null; e = e.next){
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+    private int getIndex(Object key){
+        return key.hashCode() & (map.length - 1);
+    }
+
+    private void writeObject(ObjectOutputStream s) throws Exception {
+        s.defaultWriteObject();
+        s.writeInt(count);
+        for(int i = 0; i < map.length; i++ ){
+            for(Node e = map[i]; e != null; e = e.next){
+                s.writeObject(e);
+            }
+        }
+    }
+
+    private void readObject(ObjectInputStream s) throws Exception {
+        s.defaultReadObject();
+        int n = s.readInt();
+        for(int i = 0; i < n; i++){
+            add(s.readObject());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # WikiWordCompare
-System to find similar articles based on word usage
+System to find similar articles based on word usage 
+
+## Requirements
+[Project 1](https://gee.cs.oswego.edu/dl/csc365/a1.html)
+
+This assignment asks you to create a similarity-based web-site recommendation system based on Wikipedia pages.
+- The program reads 10 (or more) Wikipedia pages. The urls for these web pages can be maintained in a control file that is read when the program starts. Use a framwwork such as JSoup to extract text bodies etc from html.
+- Establish a similarity metric, that must include information based on custom frequency tables (as will be discussed in class), possibly weighted by or in conjunction with other attributes.
+- Create a user interface that allows a user to indicate one topic or site, and displays two similar ones. The presentation details are up to you. It may be a web/browser-based (for example Spring) or GUI base (Swing, JavaFX, or Android) for the GUI. For Swing, read through the relevant parts of the Swing tutorial first.
+- Use appropriate existing libraries for every component other than the custom similarity tracking.
+
+Test your program thoroughly before submitting, and arrange a demo within 48 hours of submitting. (Demoing before submitting is strongly encouraged.)
+
+## QuickStart
+1. Download codeset
+2. Run the following command:
+```cmd
+javac '.\ObjFreqHashMap$Node.class'
+```
